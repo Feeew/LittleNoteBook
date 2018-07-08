1ea1216eba9231ee4a07774038a00254222e2424
Benjamin Seillier <fewful@hotmail.fr>
2018-07-08 17:21:43 +0200

    Remise propre

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,17 +8,17 @@ use Symfony\Component\HttpFoundation\Request;
 // for more information
 //umask(0000);
 
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
+  '127.0.0.1',
+  '::1',
+  '80.215.205.108'
+))) {
+  header('HTTP/1.0 403 Forbidden');
+  exit('This script is only accessible from localhost.');
 }
 
 require __DIR__.'/../vendor/autoload.php';
+
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
